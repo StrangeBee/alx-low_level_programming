2e8b078b66c233efb480a001c0d4d0d3c75e0094
StrangeBee <oduolabenfico@gmail.com>
2023-02-17 15:28:14 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers of base 10
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 28; i < 38; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n')
+	return (0);
+}
