8e13f4f517cbdd8848500b3b48c8f91671617b7c
StrangeBee <oduolabenfico@gmail.com>
2023-02-17 15:04:42 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets in small then big letter
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alphas[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alphas[i]);
+	}
+	putchar('\n');
+	return (0);
+}
