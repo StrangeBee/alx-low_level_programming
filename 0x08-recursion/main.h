db85fa29d792bbbec2511ef8fe011832f0ead6dd
StrangeBee <oduolabenfico@gmail.com>
2023-03-08 16:23:06 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,26 +1,15 @@
-<<<<<<< HEAD
 #ifndef MAIN_H
 #define MAIN_H
 
 void _puts_recursion(char *s);
 int _putchar(char c);
-=======
-#ifndef _MAIN_H
-#define _MAIN_H
-
-int _putchar(char);
-void _puts_recursion(char *s);
->>>>>>> 8da63c91b98ac2b6bae7cd35799fe95bfb4463f9
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-<<<<<<< HEAD
 int is_palindrome(char *s);
-=======
->>>>>>> 8da63c91b98ac2b6bae7cd35799fe95bfb4463f9
 
 
 #endif
