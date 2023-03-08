d0a7b4bb41ce67f5132d68af4c90c8ce195d62d2
StrangeBee <oduolabenfico@gmail.com>
2023-03-08 16:19:24 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: string
- * Return: no return.
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (success)
  */
 void _puts_recursion(char *s)
 {
