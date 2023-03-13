10b91323531f0d9420ff5ca9dd20b6afa4e8bf2d
StrangeBee <oduolabenfico@gmail.com>
2023-03-13 11:02:31 +0000

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check to see if there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (success)
+ */
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
 
 /**
- * main - adds positive numbers.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 (success).
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (success)
  */
+
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-	for (i = 1; i < argc; i++)
+	count = 1;
+	while (count < argc)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (check_num(argv[count]))
 		{
-			if (!isdgigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
+			str_to_int = atoi(argv[count]);
+			sum += str_t0_int;
 		}
-		add += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
 	}
-	printf("%d\n", add);
+	printf("%d\n", sum);
 	return (0);
 }
